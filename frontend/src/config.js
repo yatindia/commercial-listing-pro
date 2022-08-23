848b40e2aef4d4a66c9ef67d378df0528dfc22d6
Noel <noeljose@protonmail.com>
2022-08-23 20:09:28 +0530

    FE ports update

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,7 @@
 
 
-export const API = "http://198.58.102.188:5120"
-export const PAYMENT = "http://198.58.102.188:8120"
+export const API = "http://198.58.102.188:5000"
+export const PAYMENT = "http://198.58.102.188:8000"
 export const Client = "http://198.58.102.188:3000"
 export const IMG = "https://storage.googleapis.com/clp-image"
 export const PROF_IMG = "https://storage.googleapis.com/clp-profile-image"
